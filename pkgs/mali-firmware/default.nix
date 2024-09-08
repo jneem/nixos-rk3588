cf530b7af63d66f524acd84a55b7e452c1d6ad11
Joe Neeman <joeneeman@gmail.com>
2024-09-08 22:00:27 +0700

    Update the mali firmware url

--- pkgs/mali-firmware/default.nix
+++ pkgs/mali-firmware/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   version = "g21p0-01eac0";
 
   src = fetchurl {
-    url = "https://github.com/JeffyCN/mirrors/raw/e08ced3e0235b25a7ba2a3aeefd0e2fcbd434b68/firmware/g610/mali_csffw.bin";
+    url = "https://github.com/JeffyCN/mirrors/raw/libmali/firmware/g610/mali_csffw.bin";
     hash = "sha256-jnyCGlXKHDRcx59hJDYW3SX8NbgfCQlG8wKIbWdxLfU=";
   };
 
